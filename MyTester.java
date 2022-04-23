764b75523e59fdbc6172092364463338b6775e21
RShimkin <roma.shimkin@yandex.ru>
2022-04-23 10:56:28 +0300

    constructor

--- MyTester.java
+++ MyTester.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package shimkin.lab3;
 
 /**
- *
- * @author User
+ * тестер производительности массивов List
  */
 public class MyTester {
+    private int ops = 10000;
     
+    /**
+     * Основной конст-тор с числом операций
+     * @param num Число операций
+     */
+    public MyTester(int num){
+        ops=num;
+    }
+
+    public MyTester(){}
 }
