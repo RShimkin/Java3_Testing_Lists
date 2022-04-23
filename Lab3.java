5401177888bba7f23ae525cf41aec93fe6e793e1
RShimkin <roma.shimkin@yandex.ru>
2022-04-23 11:50:50 +0300

    testing delete, get, tail and head methods

--- Lab3.java
+++ Lab3.java
@@ -2,16 +2,29 @@ package shimkin.lab3;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Lab3 {
 
     public static void main(String[] args) {
-        MyTester tester = new MyTester(10000);
+        System.out.print("Введите количество операций: ");
+        Scanner scanner = new Scanner(System.in);
+        int ops = scanner.nextInt();
+        MyTester tester = new MyTester(ops);
         
         ArrayList<Integer> arr = new ArrayList<>();
         LinkedList<Integer> linked = new LinkedList<>();
 
         System.out.println();
-        System.out.println(String.format("Add (ArrayList -> %s ms) (LinkedList -> %s ms)", tester.testAdd(arr), tester.testAdd(linked)));
+        System.out.println(String.format("метод Add %s раз (ArrayList -> %s ms) (LinkedList -> %s ms)", 
+                tester.getOpsAmount(), tester.testAdd(arr), tester.testAdd(linked)));
+        System.out.println(String.format("метод Head %s раз (ArrayList -> %s ms) (LinkedList -> %s ms)", 
+                tester.getOpsAmount(), tester.testAddHead(arr), tester.testAddHead(linked)));
+        System.out.println(String.format("метод Tail %s раз (ArrayList -> %s ms) (LinkedList -> %s ms)", 
+                tester.getOpsAmount(), tester.testAddTail(arr), tester.testAddTail(linked)));
+        System.out.println(String.format("метод Delete %s раз (ArrayList -> %s ms) (LinkedList -> %s ms)", 
+                tester.getOpsAmount(), tester.testDelete(arr), tester.testDelete(linked)));
+        System.out.println(String.format("метод Get %s раз (ArrayList -> %s ms) (LinkedList -> %s ms)", 
+                tester.getOpsAmount(), tester.testGet(arr), tester.testGet(linked)));
     }
 }
--- MyTester.java
+++ MyTester.java
@@ -9,6 +9,14 @@ public class MyTester {
     private int ops = 10000;
     
     /**
+     * Возвращает число операций
+     * @return Число операций
+     */
+    public int getOpsAmount() {
+        return ops;
+    }
+    
+    /**
      * Основной конст-тор с числом операций
      * @param num Число операций
      */
@@ -27,6 +35,60 @@ public class MyTester {
         long start = System.currentTimeMillis();
         for(int k = 0; k<ops; ++k) {
             list.add(list.size()/2, k);
+        }
+        return System.currentTimeMillis() - start;
+    }
+    
+    /**
+     * Тестирование доступа
+     * @param list Контейнер
+     * @return Итоговое время
+     */
+    public long testGet(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        for(int k = 0; k<ops; k++) {
+            int pos = (int) Math.floor(Math.random() * ((list.size())));
+            list.get(pos);
+        }
+        return System.currentTimeMillis() - start;
+    }
+    
+    /**
+     * Тестирование добавления в начало
+     * @param list Контейнер
+     * @return Итоговое время
+     */
+    public long testAddHead(List<Integer> list){
+        long start = System.currentTimeMillis();
+        for(int k = 0; k<ops; k++) {
+            list.add(0, k);
+        }
+        return System.currentTimeMillis() - start;
+    }
+
+    /**
+     * Тестирование добавления в конец
+     * @param list Контейнер
+     * @return Итоговое время
+     */
+    public long testAddTail(List<Integer> list){
+        long start = System.currentTimeMillis();
+        for(int k = 0; k<ops; k++) {
+            list.add(k);
+        }
+        return System.currentTimeMillis() - start;
+    }
+
+    /**
+     * Тестирование удаления
+     * @param list Контейнер
+     * @return Итоговое время
+     */
+    public long testDelete(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        for(int k = 0; k<ops; k++) {
+            int pos = (int) Math.floor(Math.random() * ((list.size())));
+            list.remove(pos);
         }
         return System.currentTimeMillis() - start;
     }
